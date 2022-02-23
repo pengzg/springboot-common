68a099caa4576c9d9010d1f7d16185a00d197d25
pengzg <pzgmissyou@sina.com>
2022-02-23 17:51:22 +0800

    c

--- src/main/java/xyz/carjoy/question/utils/BaseDataTypeCodeConstants.java
+++ src/main/java/xyz/carjoy/question/utils/BaseDataTypeCodeConstants.java
@@ -5,10 +5,7 @@ public interface BaseDataTypeCodeConstants {
     public static final String type_2005 = "2005";//  1是  2否
     public static final String type_2006 = "2006"; // 1禁用  2启用
     public static final String type_2007 = "2007"; // 星期
-    public static final String type_country = "2008"; // 国家
-    public static final String type_keytype = "2009"; // 键盘类型
-    public static final String type_chargetype = "2010"; // 充电类型
-    public static final String type_system = "2011"; // 操作系统
+
 
 
 }
